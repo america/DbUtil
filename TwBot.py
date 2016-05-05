b2966abe4d3165dfdc10d60615cd6db4b5343fab
america <dreamers.ball66@gmail.com>
2016-05-05 23:14:48 +0900

    工事中...

--- TwBot.py
+++ TwBot.py
@@ -1,25 +1,35 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import twpy
-import dbUtil
-import random
 import traceback
+import re
 
 from tweepy.streaming import StreamListener
 from tweepy import Stream
 
+from twpy import *
+
 class TwListener(StreamListener):
-  def on_data(self, daga)
-    if data.startwith("{"):
-      print data
+  def on_status(self, status):
+    print("ID:", status.id)
+    print("Lang:", status.lang)
+    print("ScreenName:", status.user.screen_name)
+    print("Name:", status.user.name)
+    print("Tweet:", status.text)
+    print("Time:", status.created_at,"\n")
+
     return True
 
   def on_error(self, status):
-    print status
+    print(status)
 
-if __name__ == '__main__'
-  stream = Stream(auth, TwListener())
+if __name__ == '__main__':
+  try:
+    stream = Stream(auth, TwListener())
 
-  # get TimeLine
-  stream.userstream()
+    # get TimeLine
+    stream.userstream()
+  except KeyboardInterrupt:
+    print("動作を停止します")
+  except Exception as e:
+    traceback.print_exc()

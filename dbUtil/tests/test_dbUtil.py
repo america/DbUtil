6c6af16beeeb336e5529cb01c20e8fa6a3dbb5a0
america <dreamers.ball66@gmail.com>
2016-07-03 21:10:27 +0900

    底辺プログラマの悪あがき

--- dbUtil/tests/test_dbUtil.py
+++ dbUtil/tests/test_dbUtil.py
@@ -1,58 +1,55 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 ^*-
 
-try:
-    import unittest2 as unittest
-except (ImportError):
-    import unittest
+from nose.tools import eq_, raises
 import pymysql
 
 from dbUtil.dbUtil import dbUtil
 from constants import constants
 
 
-class test_dbUtil(unittest.TestCase):
+class test_dbUtil():
 
-    @classmethod
-    def setUpClass(cls):
-        pass
+    conn = None
 
-    def setUp(self):
+    def setup(self):
         self.conn = dbUtil.connect()
         dbUtil.create_table(self.conn, 'test_table')
 
-    def tearDown(self):
+    def teardown(self):
         dbUtil.delete_table(self.conn, 'test_table')
         dbUtil.disConnect(self.conn)
-
-    @classmethod
-    def tearDownClass(cls):
-        pass
 
     def test_connect(self):
         expected = pymysql.connections.Connection
         _conn = dbUtil.connect()
-        self.assertIsInstance(_conn, expected)
+        assert isinstance(_conn, expected)
         dbUtil.disConnect(_conn)
 
+    @raises(IOError)
     def test_connect_err(self):
         constants.DB_INFO_INI = 'not_exist_file'
-        self.assertRaises(IOError, dbUtil.connect)
-        constants.DB_INFO_INI = 'db_info.ini'
+        try:
+            dbUtil.connect()
+        finally:
+            constants.DB_INFO_INI = 'db_info.ini'
 
+    # @with_setup(setup_func, teardown_func)
     def test_create_table(self):
         expected = True
         actual = dbUtil.create_table(self.conn, 'case_table')
-        self.assertEqual(actual, expected)
+        eq_(actual, expected)
         dbUtil.delete_table(self.conn, 'case_table')
 
+    # @with_setup(setup_func, teardown_func)
     def test_insert_message(self):
         expected = 1
 
         actual = dbUtil.insert_message(self.conn, 'test_table', 'test_message')
 
-        self.assertEqual(actual, expected)
+        eq_(actual, expected)
 
+    # @with_setup(setup_func, teardown_func)
     def test_getAllMsgs(self):
 
         expected = (1, 'test_message')
@@ -61,31 +58,30 @@ class test_dbUtil(unittest.TestCase):
 
         actual = (result_json[0]['NO'], result_json[0]['CONTENTS'])
 
-        self.assertEqual(actual, expected)
+        eq_(actual, expected)
 
+    # @with_setup(setup_func, teardown_func)
     def test_delete_message(self):
         expected = True
         dbUtil.insert_message(self.conn, 'test_table', 'test_message')
         actual = dbUtil.delete_message(self.conn, 'test_table', 1)
-        self.assertEqual(actual, expected)
+        eq_(actual, expected)
 
+    # @with_setup(setup_func, teardown_func)
     def test_delete_message_err(self):
         expected = False
         actual = dbUtil.delete_message(self.conn, 'test_table', 2)
-        self.assertEqual(actual, expected)
+        eq_(actual, expected)
 
+    # @with_setup(setup_func, teardown_func)
     def test_delete_table(self):
         expected = True
         dbUtil.create_table(self.conn, 'test_table_for_delete_table')
         actual = dbUtil.delete_table(self.conn, 'test_table_for_delete_table')
-        self.assertEqual(actual, expected)
+        eq_(actual, expected)
 
-    def test_disConnect(self):
+    def test_diconnect(self):
         expected = True
-        _conn = dbUtil.connect()
-        actual = dbUtil.disConnect(_conn)
-        self.assertEqual(actual, expected)
-
-if __name__ == '__main__':
-
-    unittest.main()
+        conn = dbUtil.connect()
+        actual = dbUtil.disConnect(conn)
+        eq_(actual, expected)
